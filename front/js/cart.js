2b07942eea5fa5785a6501dfa156eb271eab9c3f
leo-sct <leo.sichet.github@gmail.com>
2022-11-08 15:21:08 +0100

    prixqtt

--- front/js/cart.js
+++ front/js/cart.js
@@ -17,7 +17,7 @@ if (prdctStorage) {
                 console.log(B);
                 document.getElementById(
                     "cart__items"
-                ).innerHTML += `<article class="cart__item" data-id="${B._id}" data-color="${B.colors}">
+                ).innerHTML += `<article class="cart__item" data-id="${a._ID}" data-color="${a.color}">
                 <div class="cart__item__img">
                   <img src="${B.imageUrl}" alt="Photographie d'un canapé">
                 </div>
@@ -30,7 +30,7 @@ if (prdctStorage) {
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
                       <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${a.qty$}">
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${a.qty}">
                     </div>
                     <div class="cart__item__content__settings__delete">
                       <p class="deleteItem">Supprimer</p>
@@ -38,6 +38,49 @@ if (prdctStorage) {
                   </div>
                 </div>
               </article>`;
+            })
+
+            .then(() => {
+                suppcanap();
+                tqprix();
             });
+        function suppcanap() {
+            const supprimerbtn = document.getElementsByClassName("deleteItem");
+            for (const deletebtn of supprimerbtn) {
+                deletebtn.addEventListener("click", () => {
+                    deletebtn.closest("article").remove();
+                    let color = deletebtn.closest("article").dataset.color;
+                    let id = deletebtn.closest("article").dataset.id;
+                    let index = -1;
+                    prdctStorage.find((item) => {
+                        if (item.color == color && item._ID == id) {
+                            index = prdctStorage.indexOf(item);
+                        }
+                    });
+                    if (index !== -1) {
+                        prdctStorage.splice(index, 1);
+                        localStorage.setItem("product", JSON.stringify(prdctStorage));
+                        console.log(prdctStorage);
+                    }
+                    tqprix();
+                });
+            }
+        }
+
+        function tqprix() {
+            let quantity = document.getElementsByClassName("itemQuantity");
+            let totalquantity = 0;
+            let totalprix = 0;
+            for (let i = 0; i < quantity.length; i++) {
+                totalquantity += parseInt(quantity[i].value);
+            }
+            document.getElementById("totalQuantity").innerText = totalquantity;
+            const description = document.getElementsByClassName("cart__item__content__description");
+            for (let i = 0; i < description.length; i++) {
+                const prixarticle = description[i].lastElementChild.innerHTML;
+                totalprix += parseInt(quantity[i].value) * parseInt(prixarticle);
+            }
+            document.getElementById("totalPrice").innerText = totalprix;
+        }
     });
 }
